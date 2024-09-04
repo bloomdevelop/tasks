d183e7bca5cf7f44ad67a51d8a9829c44723997a
Bloom <bloomdevelop@proton.me>
2024-09-04 11:44:05 -0400

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,396 @@
+import { createSignal, For, onMount, Show, Suspense } from "solid-js";
+import "./App.css";
+import localforage from "localforage";
+import {
+  FaSolidEllipsis,
+  FaSolidInfo,
+  FaSolidNoteSticky,
+  FaSolidPen,
+  FaSolidPlus,
+  FaSolidTag,
+  FaSolidTags,
+  FaSolidTrashCan
+} from "solid-icons/fa";
+import Dialog from "@corvu/dialog";
+import Tooltip from "@corvu/tooltip";
+import { DropdownMenu } from "@kobalte/core/dropdown-menu";
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+function App() {
+  const [todosSignal, setTodosSignal] = createSignal<any>();
+  const [tagsSignal, setTagsSignal] = createSignal<any>();
+  const [tagsInputSignal, setTagsInputSignal] = createSignal<string>("");
+  const [inputSignal, setInputSignal] = createSignal<string>("");
+  onMount(async () => {
+    injectSpeedInsights();
+    const todosData = await localforage.getItem("todos");
+    const tagsData = await localforage.getItem("tags");
+    if (!tagsData) {
+      await localforage.setItem("tags", []);
+    }
+    if (!todosData) {
+      await localforage.setItem("todos", []);
+    }
+    setTagsSignal(tagsData);
+    setTodosSignal(todosData);
+  });
+
+  const createTodo = async () => {
+    const todos = (await localforage.getItem("todos")) as [];
+    if (todos) {
+      await localforage.setItem("todos", [
+        ...todos,
+        {
+          name: inputSignal(),
+          completed: false,
+          tags: [],
+        },
+      ]);
+      setTodosSignal(await localforage.getItem("todos"));
+      setInputSignal("");
+    }
+  };
+
+  const createTag = async (tag: string) => {
+    const tags = (await localforage.getItem("tags")) as [];
+    const rng = Math.floor(Math.random() * 7) + 1;
+    let color;
+    if (rng === 1) {
+      color = "adw-blue";
+    } else if (rng === 2) {
+      color = "adw-green";
+    } else if (rng === 3) {
+      color = "adw-red";
+    } else if (rng === 4) {
+      color = "adw-yellow";
+    } else if (rng === 5) {
+      color = "adw-purple";
+    } else if (rng === 6) {
+      color = "adw-brown";
+    } else if (rng === 7) {
+      color = "adw-orange";
+    }
+    if (tags) {
+      await localforage.setItem("tags", [
+        ...tags,
+        {
+          name: tag,
+          color: color,
+        },
+      ]);
+      setTagsSignal(await localforage.getItem("tags"));
+      setTagsInputSignal("");
+    }
+  };
+
+  const addTagToTodo = async (index: number, tag: any) => {
+    const todos = (await localforage.getItem("todos")) as [];
+    if (todos) {
+      const updatedTodos = [...todos] as any;
+      updatedTodos[index].tags.push(tag);
+      await localforage.setItem("todos", updatedTodos);
+      setTodosSignal(await localforage.getItem("todos"));
+    }
+  };
+
+  const removeTag = async (index: number) => {
+    const todos = (await localforage.getItem("todos")) as [];
+    if (todos) {
+      const updatedTodos = [...todos] as any;
+      updatedTodos[index].tags.splice(index, 1);
+      await localforage.setItem("todos", updatedTodos);
+      setTodosSignal(await localforage.getItem("todos"));
+    }
+    const tags = (await localforage.getItem("tags")) as [];
+    if (tags) {
+      const updatedTags = [...tags] as any;
+      updatedTags.splice(index, 1);
+      await localforage.setItem("tags", updatedTags);
+      setTagsSignal(await localforage.getItem("tags"));
+    }
+  };
+
+  const removeTagFromTodo = async (index: number) => {
+    const todos = (await localforage.getItem("todos")) as [];
+    if (todos) {
+      const updatedTodos = [...todos] as any;
+      updatedTodos[index].tags.splice(index, 1);
+      await localforage.setItem("todos", updatedTodos);
+      setTodosSignal(await localforage.getItem("todos"));
+    }
+  };
+
+  return (
+    <>
+      <div class="container">
+        <div class="toolbar">
+          <Dialog>
+            <Tooltip
+              placement="bottom"
+              floatingOptions={{ offset: 5 }}
+              strategy="absolute"
+            >
+              <Tooltip.Anchor>
+                <Tooltip.Trigger as={"div"}>
+                  <Dialog.Trigger>
+                    <FaSolidInfo />
+                  </Dialog.Trigger>
+                </Tooltip.Trigger>
+              </Tooltip.Anchor>
+              <Tooltip.Portal>
+                <Tooltip.Content>Info</Tooltip.Content>
+              </Tooltip.Portal>
+            </Tooltip>
+            <Dialog.Portal>
+              <Dialog.Overlay />
+              <Dialog.Content>
+                <div class="d-grid">
+                  <img src={"/appIcon.svg"} alt="App Icon" />
+                  <h1>Tasks</h1>
+                  <p>Developed by Bloom Perez</p>
+                  <p>
+                    Made with <code>corvu</code>, <code>solid-js</code>,{" "}
+                    <code>localforage</code> and <code>solid-icons</code>
+                  </p>
+                </div>
+              </Dialog.Content>
+            </Dialog.Portal>
+          </Dialog>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              if (inputSignal().length === 0) return;
+              createTodo();
+            }}
+          >
+            <input
+              value={inputSignal()}
+              onChange={(e) => setInputSignal(e.currentTarget.value)}
+              type="text"
+              placeholder="Add a task"
+              class="input"
+            />
+            <button type="submit">
+              <FaSolidPlus /> Create
+            </button>
+          </form>
+        </div>
+        <div class="todos">
+          <Suspense
+            fallback={<div style={{ "text-align": "center" }}>Loading...</div>}
+          >
+            <For
+              each={todosSignal()}
+              fallback={
+                <div
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    display: "flex",
+                    "flex-direction": "column",
+                    "justify-content": "center",
+                    "align-items": "center",
+                  }}
+                >
+                  <FaSolidNoteSticky size={50} />
+                  <p>There's no tasks in this moment</p>
+                </div>
+              }
+            >
+              {(todo, index) => {
+                return (
+                  <div class="todo">
+                    <div class="flex-row">
+                      <input
+                        class="circled-checkbox"
+                        type="checkbox"
+                        id={`todo-${index()}`}
+                        checked={todo.completed}
+                        onChange={() => {
+                          const updatedTodos = [...todosSignal()];
+                          updatedTodos[index()].completed =
+                            !updatedTodos[index()].completed;
+                          setTodosSignal(updatedTodos);
+                          localforage.setItem("todos", updatedTodos);
+                        }}
+                      />
+                      <div class="todo-content">
+                        <h1>{todo.name}</h1>
+                        <Show when={todo.tags.length > 0}>
+                          <div
+                            style={{
+                              display: "flex",
+                              "flex-direction": "row",
+                              gap: ".5rem",
+                            }}
+                          >
+                            <For each={todo.tags}>
+                              {(tag, index) => {
+                                return (
+                                  <div
+                                    style={{
+                                      display: "flex",
+                                      "flex-direction": "row",
+                                      "align-items": "center",
+                                      color: "inherit",
+                                      gap: ".4rem",
+                                      "border-radius": "5px",
+                                      padding: ".2rem .5rem",
+                                      cursor: "pointer"
+                                    }}
+                                    onClick={() => removeTagFromTodo(index())}
+                                    class={`${tag.color}`}
+                                  >
+                                    <FaSolidTag />
+                                    <p>{tag.name}</p>
+                                  </div>
+                                );
+                              }}
+                            </For>
+                          </div>
+                        </Show>
+                      </div>
+                    </div>
+                    <div
+                      style={{
+                        display: "flex",
+                        "flex-direction": "row",
+                        gap: ".5rem",
+                      }}
+                    >
+                      <DropdownMenu gutter={5}>
+                        <DropdownMenu.Trigger>
+                          <FaSolidEllipsis />
+                        </DropdownMenu.Trigger>
+                        <DropdownMenu.Portal>
+                          <DropdownMenu.Content class="dropdown-menu__content">
+                            <div
+                              style={{
+                                display: "flex",
+                                "flex-direction": "column",
+                                gap: ".2rem",
+                              }}
+                            >
+                              <DropdownMenu.Item class="dropdown-menu__item">
+                                <FaSolidPen /> Remane
+                              </DropdownMenu.Item>
+                              <DropdownMenu.Sub gutter={10}>
+                                <DropdownMenu.SubTrigger
+                                  as={"div"}
+                                  class="dropdown-menu__item"
+                                >
+                                  <FaSolidTags /> Add Tags
+                                </DropdownMenu.SubTrigger>
+                                <DropdownMenu.Portal>
+                                  <DropdownMenu.SubContent class="dropdown-menu__subcontent">
+                                    <div
+                                      style={{
+                                        display: "flex",
+                                        "flex-direction": "column",
+                                        gap: ".3rem",
+                                      }}
+                                    >
+                                      <For each={tagsSignal()}>
+                                        {(tag) => (
+                                          <div
+                                            style={{
+                                              display: "flex",
+                                              "flex-direction": "row",
+                                              gap: ".3rem",
+                                            }}
+                                          >
+                                            <DropdownMenu.Item
+                                              style={{ "flex-grow": 1 }}
+                                              class="dropdown-menu__item"
+                                              onClick={() => {
+                                                addTagToTodo(index(), tag);
+                                              }}
+                                            >
+                                              <div
+                                                style={{
+                                                  width: "15px",
+                                                  height: "15px",
+                                                  "border-radius": "50%",
+                                                  display: "inline-block",
+                                                }}
+                                                class={`${tag.color}`}
+                                              />
+
+                                              {tag.name}
+                                            </DropdownMenu.Item>
+                                            <button
+                                              onClick={() => {
+                                                removeTag(index());
+                                              }}
+                                              class="delete small"
+                                            >
+                                              <FaSolidTrashCan />
+                                            </button>
+                                          </div>
+                                        )}
+                                      </For>
+                                      <Show when={tagsSignal().length > 0}>
+                                        <DropdownMenu.Separator class="dropdown-menu__separator" />
+                                      </Show>
+                                      <form
+                                        style={{
+                                          display: "flex",
+                                          "flex-direction": "row",
+                                          gap: ".2rem",
+                                        }}
+                                        onSubmit={async (e) => {
+                                          e.preventDefault();
+                                          createTag(tagsInputSignal());
+                                        }}
+                                      >
+                                        <input
+                                          style={{
+                                            width: "100%",
+                                          }}
+                                          placeholder="Add new tag"
+                                          type="text"
+                                          value={tagsInputSignal()}
+                                          onChange={(e) => {
+                                            setTagsInputSignal(e.target.value);
+                                          }}
+                                        />
+                                        <button type="submit">
+                                          <FaSolidPlus />
+                                        </button>
+                                      </form>
+                                    </div>
+                                  </DropdownMenu.SubContent>
+                                </DropdownMenu.Portal>
+                              </DropdownMenu.Sub>
+                            </div>
+                          </DropdownMenu.Content>
+                        </DropdownMenu.Portal>
+                      </DropdownMenu>
+                      <button
+                        class="delete"
+                        onClick={async () => {
+                          const todos = (await localforage.getItem(
+                            "todos"
+                          )) as [];
+                          const updatedTodos = todos.filter(
+                            (_, i) => i !== index()
+                          );
+                          setTodosSignal(updatedTodos);
+                          localforage.setItem("todos", updatedTodos);
+                        }}
+                      >
+                        <FaSolidTrashCan /> Delete
+                      </button>
+                    </div>
+                  </div>
+                );
+              }}
+            </For>
+          </Suspense>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import solid from "vite-plugin-solid";
+
+export default defineConfig({
+  plugins: [
+    solid(),
+    VitePWA({
+      registerType: "prompt",
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon-180x180.png", "maskabke-icon-512x512.svg"],
+      manifest: {
+        name: "Tasks",
+        short_name: "Tasks",
+        description: "A extremly simple todo app created using solid-js",
+        categories: ["productivity"],
+        display: "standalone",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
